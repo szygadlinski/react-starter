a412d59244bed264ee1d5c9fcd9c6b340369c8a3
Szymon Zygadliński <szygadlinski@gmail.com>
2021-01-29 23:00:20 +0100

    Implement card filtering

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './App.scss';
 import List from '../List/ListContainer.js';
+import Search from '../Search/SearchContainer.js';
 //import Creator from '../Creator/Creator.js';
 //import {settings} from '../../data/dataStore.js';
 
@@ -18,6 +19,8 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
+
+        <Search />
 
         {lists.map((listData) => (
           <List key={listData.id} {...listData} />
